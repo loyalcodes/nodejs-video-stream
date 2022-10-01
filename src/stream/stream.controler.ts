ec0d9a9bed819059c5103aee10f085d5261c9ccc
loyalcodes <loyalcodestudio@gmail.com>
2022-10-01 10:21:05 +0200

    Added stream capabilities to streamService

--- src/stream/stream.controler.ts
+++ src/stream/stream.controler.ts
@@ -1,4 +1,16 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Header, Headers, Res, HttpStatus } from "@nestjs/common";
+import { StreamService } from "./stream.service";
+import { Response } from "express";
 
-@Controller('stream')
-export class StreamControler {}
+@Controller()
+export class StreamControler {
+ constructor(private readonly streamService: StreamService){}
+
+    @Get('stream')
+    @Header('Accept-Ranges', 'bytes')
+    @Header('Content-Type', 'video/mp4')
+    initStream(@Headers() headers, @Res() res: Response){
+       return this.streamService.start(headers, res);
+    }
+
+}
--- src/stream/stream.service.ts
+++ src/stream/stream.service.ts
@@ -1,5 +1,37 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, StreamableFile, Header, Headers, Res, HttpStatus, Get, NotFoundException } from "@nestjs/common";
+import { Response } from "express";
+import { statSync, createReadStream } from 'fs';
+import { join } from "path";
 
 //Inject the service
 @Injectable()
-export class StreamService {}
+export class StreamService {
+    
+    async start( @Headers() headers, @Res() res: Response){
+    const videoPath = join(process.cwd(), `./src/assets/video.mp4`)
+    if(!videoPath){
+        throw new NotFoundException(null, "Streamable file not found.")
+    }
+    const { size } = statSync(videoPath);
+    const videoRange = headers.range;
+    if (videoRange) {
+      const parts = videoRange.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : size - 1;
+      const chunkSize = (end - start) + 1;
+      const readStreamfile = createReadStream(videoPath, { start, end, highWaterMark:60 });
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${size}`,
+        'Content-Length': chunkSize,
+      };
+      res.writeHead(HttpStatus.PARTIAL_CONTENT, head);
+      readStreamfile.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': size,
+      };
+      res.writeHead(HttpStatus.OK, head);
+      createReadStream(videoPath).pipe(res);
+    }
+  } 
+}
