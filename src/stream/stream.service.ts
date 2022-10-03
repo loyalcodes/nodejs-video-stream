44109587a2edd59b038227b614be101acb48607f
loyalcodes <loyalcodestudio@gmail.com>
2022-10-03 14:15:01 +0200

    Fixed video connvert

--- src/stream/stream.service.ts
+++ src/stream/stream.service.ts
@@ -1,11 +1,7 @@
 import { Injectable, StreamableFile, Header, Headers, Res, HttpStatus, Get, NotFoundException, OnApplicationShutdown, Logger } from "@nestjs/common";
 import { Response } from "express";
-import { statSync, createReadStream } from 'fs';
-import { join } from "path";
+import { statSync, createReadStream, createWriteStream } from 'fs';
 import { MediaUtils } from "src/utils/utils.media";
-const Transcoder = require('stream-transcoder');
-const ffmpeg = require('fluent-ffmpeg');
-
 
 
 //Inject the service
@@ -17,49 +13,63 @@ export class StreamService implements OnApplicationShutdown{
     }
 
     private logger = new Logger("StreamService")
-    
-    async start( @Headers() headers, @Res() res: Response){ // Start video streaming while waiting on resources
-    this.logger.verbose(`Started streaming for ${process.pid}`)
+
+    async start( @Headers() headers, @Res() res: Response){
+
+      const mediaUtils = new MediaUtils()
+      const streamableMediaPath = mediaUtils.getStreamableMedia()
+      const readStream = this.readStreamData()
+      const writableStream = createWriteStream(streamableMediaPath);
+
+      console.log(`===================READ STREAM DATA ==============`)
+
+      readStream.on('data', (chunk) =>{
+          writableStream.write(chunk);
+          console.log(chunk) 
+      });
+
+      readStream.on('end', ()=>{
+
+        console.log(" Completed .mp4 to .mkv ")
+
+        const { size } = statSync(streamableMediaPath);
+        const streamableFileRange = headers.range;
+
+        if(streamableFileRange){
+          const parts = streamableFileRange.replace(/bytes=/, "").split("-");
+          const start = parseInt(parts[0], 10);
+          const end = parts[1] ? parseInt(parts[1], 10) : size - 1;
+          const chunkSize = (end - start) + 1;
+
+          const readStreamfile = createReadStream(streamableMediaPath, { start, end, highWaterMark:60 });
+          const head = {
+            'Content-Range': `bytes ${start}-${end}/${size}`,
+            'Content-Length': chunkSize,
+            'Content-Type': 'video/mp4'
+          };
+          res.writeHead(HttpStatus.PARTIAL_CONTENT, head); // Expose data to client
+          readStreamfile.pipe(res);
+          console.log(`=================== HEADER CONTENT ==============`)
+          console.log(`parts: ${parts} start: ${start}`)
+        }else{
+
+          const head = {
+            'Content-Length': size,
+          };
+
+          res.writeHead(HttpStatus.OK, head);
+          createReadStream(streamableMediaPath).pipe(res);
+          console.log(`Header content: ${JSON.stringify(head)}`)
+        }
+       })
+       readStream.on('error', (error)=>{
+          console.log(`ReadStreamDataError: ${error}`)
+       })
+  }
+
+  readStreamData(){ //Get stream data from mp4
     const mediaUtils = new MediaUtils()
-    const videoPath = mediaUtils.getMedia()
-    const { size } = statSync(videoPath);
-    const videoRange = headers.range;
-    let parts: any, start: any, chunkSize: any, end: any = null;
-    if (videoRange) {
-       parts = videoRange.replace(/bytes=/, "").split("-");
-       start = parseInt(parts[0], 10);
-       end = parts[1] ? parseInt(parts[1], 10) : size - 1;
-       chunkSize = (end - start) + 1;
-      const readStreamfile = createReadStream(videoPath, { start, end, highWaterMark:60 });
-      const head = {
-        'Content-Range': `bytes ${start}-${end}/${size}`,
-        'Content-Length': chunkSize,
-        'Content-Type': 'video/mp4'
-      };
-      res.writeHead(HttpStatus.PARTIAL_CONTENT, head); // Expose data to client
-      readStreamfile.pipe(res);
-      
-      this.logger.verbose(`Current streaming data: ${JSON.stringify(
-        {
-            "parts": parts,
-            "chunkSize": chunkSize,
-            "end": end
-        }
-      )}`)
-    } else {
-      const head = {
-        'Content-Length': size,
-      };
-      res.writeHead(HttpStatus.OK, head);
-      createReadStream(videoPath).pipe(res); // Expose StreamableData
-      this.logger.verbose(`Current streaming data: ${JSON.stringify(
-        {
-            "parts": parts,
-            "chunkSize": chunkSize,
-            "end": end
-        }
-      )}`)
-    }
+    return createReadStream(mediaUtils.getMedia());
   }
 
 }
--- src/utils/utils.media.ts
+++ src/utils/utils.media.ts
@@ -1,6 +1,6 @@
 import { NotFoundException } from "@nestjs/common"
+import { createWriteStream } from "fs";
 import { join } from "path"
-import Transcoder from 'stream-transcoder';
 
 export class MediaUtils{
      getMedia (): any {
@@ -8,22 +8,8 @@ export class MediaUtils{
         if(filePath) return filePath
         return new NotFoundException(null, "Streamable file not found")
      }
-    
-    mediaTranscoder(){
-       return new Transcoder(null)
-    	    .maxSize(320, 240)
-    	    .videoCodec('h264')
-    	    .videoBitrate(800 * 1000)
-    	    .fps(25)
-    	    .audioCodec('libfaac')
-    	    .sampleRate(44100)
-    	    .channels(2)
-    	    .audioBitrate(128 * 1000)
-    	    .format('mkv')
-    	    .on('finish', function() {
-    	    	
-    	    })
-    	    .stream().pipe(null);
-    }
-    
+
+	 getStreamableMedia (): any {
+        return join(process.cwd(), `./src/assets/sample.mkv`)
+     }
 }
